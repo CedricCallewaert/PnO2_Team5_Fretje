49012165287a9423cc9713393caa3b9b3eb89d6a
Maxime Van Insberghe <maxime.vaninsberghe@student.kuleuven.be>
2023-03-11 14:34:17 +0100

    Update programma
    3 detecteren

--- Color_Detection.py
+++ Color_Detection.py
@@ -20,45 +20,110 @@ while True:
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
     contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
+
+    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
+
     center = None
     
     if len(contours) > 0:
+
+        ###first circle
         #get the max contour
-        c = max(contours, key = cv2.contourArea)
-        
-        # transform into rectangle
-        circle = cv2.minEnclosingCircle(c)
-        
-        ((x, y), radius) = circle
-        
-        s = "x: {}, y: {}, radius: {}".format(np.round(x), np.round(y), np.round(radius)) 
-        print(s)
-        
-        #box
-        center = (int(x),int(y))
-        radius = int(radius)
-        
+        c = sorted_contours[0]
+
+        # transform into circle
+        circle1 = cv2.minEnclosingCircle(c)
+        ((x1, y1), radius1) = circle1
+        s1 = "x1: {}, y1: {}, radius1: {}".format(np.round(x1), np.round(y1), np.round(radius1))
+
+        #circle
+        center1 = (int(x1),int(y1))
+        radius1 = int(radius1)
+
         #moment
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        
+        M1 = cv2.moments(c)
+        if M1["m00"] == 0:
+            M1["m00"] = 1.0
+        center1 = (int(M1["m10"] / M1["m00"]), int(M1["m01"] / M1["m00"]))
+
         # draw contour : yellow
-        cv2.circle(frame,center,radius,(0,255,0),2)
-        
+        cv2.circle(frame,center1,radius1,(0,255,0),2)
+
         # draw a dot to the center : pink
-        cv2.circle(frame, center, 5, (255, 0, 255), -1)
-        
+        cv2.circle(frame, center1, 5, (255, 0, 255), -1)
+
         # write to the screen
-        cv2.putText(frame, s, (25, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
+        cv2.putText(frame, s1, (25, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
+
+
+
+
+    if len(contours) > 1:
+
+
+
+        ###second circle
+        #get the max contour
+        d = sorted_contours[1]
+
+        # transform into circle
+        circle2 = cv2.minEnclosingCircle(d)
+        ((x2, y2), radius2) = circle2
+        s2 = "x2: {}, y2: {}, radius2: {}".format(np.round(x2), np.round(y2), np.round(radius2)) 
+
+        #circle
+        center2 = (int(x2),int(y2))
+        radius2 = int(radius2)
+
+        #moment
+        M2 = cv2.moments(d)
+        if M2["m00"] == 0:
+            M2["m00"] = 1.0
+        center2 = (int(M2["m10"] / M2["m00"]), int(M2["m01"] / M2["m00"]))
+
+        # draw contour : yellow
+        cv2.circle(frame,center2,radius2,(0,255,0),2)
+
+        # draw a dot to the center : pink
+        cv2.circle(frame, center2, 5, (255, 0, 255), -1)
+
+        # write to the screen
+        cv2.putText(frame, s2, (25, 75), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
+
+
+
+    if len(contours) > 2:
+
+
+        ###third circle
+        #get the max contour
+        e = sorted_contours[2]
+
+        # transform into circle
+        circle3 = cv2.minEnclosingCircle(e)
+        ((x3, y3), radius3) = circle3
+        s3 = "x3: {}, y3: {}, radius3: {}".format(np.round(x3), np.round(y3), np.round(radius3)) 
+
+        #circle
+        center3 = (int(x3),int(y3))
+        radius3 = int(radius3)
+
+        #moment
+        M3 = cv2.moments(e)
+        if M3["m00"] == 0:
+            M3["m00"] = 1.0
+        center3 = (int(M3["m10"] / M3["m00"]), int(M3["m01"] / M3["m00"]))
+
+        # draw contour : yellow
+        cv2.circle(frame,center3,radius3,(0,255,0),2)
+
+        # draw a dot to the center : pink
+        cv2.circle(frame, center3, 5, (255, 0, 255), -1)
+
+        # write to the screen
+        cv2.putText(frame, s3, (25, 100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
 
     cv2.imshow("Frame", frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-
-
-    
-
 
     key = cv2.waitKey(1)
     if key == 27:
