16d53bc5e9cf07b3a963e97573e291c9f73b1a02
Maxime Van Insberghe <maxime.vaninsberghe@student.kuleuven.be>
2023-03-31 12:48:50 +0200

    verplaatsing foro

--- LabVIEW/Python test/Color_Detection.py
+++ LabVIEW/Python test/Color_Detection.py
@@ -1,6 +1,47 @@
 import cv2 
 import numpy as np
 
+def distance_to_camera_alternative(height, y_waarde, x_waarde):
+    y_graden= y_waarde * (18/720) + 50,2
+    x_graden= x_waarde * (75/1280)
+
+def camera_calibration():
+    
+    # Define the calibration pattern parameters
+    pattern_size = (5, 5)  # aantal hoekpunten
+    square_size = 0.027     # oppervlakte van een vierkantje in meter
+
+    
+    obj_points = []  # 3D punten
+    img_points = []  # 2D plunten
+
+    # Load images
+    cap = cv2.VideoCapture(0)
+    images = []
+    for i in range(10):
+        _, frame = cap.read()
+        images.append(frame)
+    cap.release()
+
+    # Define the object points for the calibration pattern (in the calibration pattern coordinate system)
+    objp = np.zeros((pattern_size[0]*pattern_size[1], 3), np.float32)
+    objp[:,:2] = np.mgrid[0:pattern_size[0],0:pattern_size[1]].T.reshape(-1,2) * square_size
+
+    # Find the corners in the calibration images 
+    for img in images:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
+
+        if ret == True:
+            obj_points.append(objp)
+            img_points.append(corners)
+
+    # Calibrate the camera using the object points and image points
+    ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+
+    # Print the camera matrix
+    return K, dist, rvecs, tvecs
+    print(K)
 
 
 def distance_to_camera(knownWidth, focalLength , perWidth):
@@ -125,4 +166,5 @@ def main(aantal):
 
     return output
 
-main(10000000)
+
+print(camera_calibration())
