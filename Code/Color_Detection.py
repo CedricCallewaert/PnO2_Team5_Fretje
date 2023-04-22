3e8ff5ed3aa3a9621c93eaf11cb239989b8ee91c
Maxime Van Insberghe <maxime.vaninsberghe@student.kuleuven.be>
2023-04-22 08:33:22 +0200

    aanpassing

--- Code/Color_Detection.py
+++ Code/Color_Detection.py
@@ -3,10 +3,6 @@ import numpy as np
 import glob
 
 
-def distance_to_camera_alternative(height, y_waarde, x_waarde):
-    y_graden= y_waarde * (18/720) + 50,2
-    x_graden= x_waarde * (75/1280)
-
 def getImages():
     cap = cv2.VideoCapture(0)
 
@@ -32,10 +28,49 @@ def getImages():
 
     cv2.destroyAllWindows()
 
+def camera_pose_estimation(K,dist):
+
+    cap = cv2.VideoCapture(0)
+    _, img = cap.read()
+
+    # Release and destroy all windows before termination
+    cap.release()
+
+    # chessboard parameters
+    chessboardSize = (8, 6)
+    squareSize = 21
+    frameSize = (1920, 1080)
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points
+    objp = np.zeros((chessboardSize[0] * chessboardSize[1],3), np.float32)
+    objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+
+    objp = objp * squareSize
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    
+    # Find the chess board corners
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, chessboardSize ,None)
+
+    success, rvec, tvec = cv2.solvePnP(objp, corners, K, dist)
+    
+    rot_mat, _ = cv2.Rodrigues(rvec)
+    
+    projection_matrix = np.hstack((rot_mat, tvec))
+    
+    return projection_matrix
+    
+
 def camera_calibration():
     # chessboard parameters
-    chessboardSize = (9, 5)
-    squareSize = 20
+    chessboardSize = (8, 6)
+    squareSize = 21
     frameSize = (1920, 1080)
 
 
@@ -43,7 +78,7 @@ def camera_calibration():
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    # prepare object points
     objp = np.zeros((chessboardSize[0] * chessboardSize[1],3), np.float32)
     objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
 
@@ -166,12 +201,8 @@ def draw_circle(contours, number, frame):
     # draw a dot to the center : pink
     cv2.circle(frame, center1, 5, (255, 0, 255), -1)
 
-    # calculate distance
-    
-    distance1 = distance_to_camera(85, 1425, radius1)
-    
     #create text
-    s1 = "x1: {}, y1: {}, radius1: {}, distance1: {}".format(np.round(x1), np.round(y1), np.round(radius1), distance1)
+    s1 = "x1: {}, y1: {}, radius1: {}".format(np.round(x1), np.round(y1), np.round(radius1))
     # write to the screen
     cv2.putText(frame, s1, (25, 50 + number * 25), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
 
@@ -181,15 +212,17 @@ def draw_circle(contours, number, frame):
 
 
 
-def main(aantal):
+def main():
     # Get the camera calibration matrix and distortion coefficients
-    K, dist = camera_calibration()
+    K= np.array(([[1.48635604e+03, 0.00000000e+00, 9.79901544e+02],
+       [0.00000000e+00, 1.48230147e+03, 4.95819480e+02],
+       [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]))
 
-    # get the coordinates of the 3D points and the 2D points
-    coords_3d, coords_2d = get_coordinates()
+    dist= np.array([[ 0.06309409, -0.20227065, -0.00725773,  0.00190645,  0.33597095]])
+
 
     # Calculate the projection matrix
-    projection_matrix = calculate_projection_matrix(K, dist, coords_3d, coords_2d)
+    projection_matrix = np.array([[ 1.04390024e-01, -9.94436049e-01 , 1.41303371e-02 , 7.94931404e+01],[ 9.77768968e-01 , 1.05217649e-01 , 1.81375553e-01, -1.38314246e+02],[-1.81853149e-01 ,-5.11759323e-03 , 9.83312383e-01,  1.15914001e+03]])
 
 
     gemiddelde1 = 0
@@ -209,45 +242,45 @@ def main(aantal):
     teller3=0
 
 
-    for i in range(aantal):
+    
 
-        _, frame = cap.read()
+    _, frame = cap.read()
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    
+    
+    lower_red_1 = np.array([0,0,220]) 
+    upper_red_1 = np.array([179,50,255])
+
+    mask = cv2.inRange(hsv, lower_red_1, upper_red_1)
+
+    contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
+    
+    
+    if len(contours) > 0:
+        teller1+=1
+        som1 += draw_circle(sorted_contours, 0, frame)
         
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
+    if len(contours) > 1:
+        teller2+=1
+        som2 += draw_circle(sorted_contours, 1, frame)
         
-        
-        lower_red_1 = np.array([0,0,220]) 
-        upper_red_1 = np.array([179,50,255])
-
-        mask = cv2.inRange(hsv, lower_red_1, upper_red_1)
-
-        contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
-        
-        
-        if len(contours) > 0:
-            teller1+=1
-            som1 += draw_circle(sorted_contours, 0, frame)
-          
-        if len(contours) > 1:
-            teller2+=1
-            som2 += draw_circle(sorted_contours, 1, frame)
-           
-        if len(contours) > 2:
-            teller3+=1
-            som3 += draw_circle(sorted_contours, 2, frame)
-           
-
+    if len(contours) > 2:
+        teller3+=1
+        som3 += draw_circle(sorted_contours, 2, frame)
         
 
-        cv2.imshow("Frame", frame)
-        cv2.imshow("Mask", mask)
+    
 
-        key = cv2.waitKey(1)
-        
-        i += 1
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1)
+    
+    i += 1
 
 
 
@@ -257,12 +290,6 @@ def main(aantal):
     cv2.destroyAllWindows()
 
     
-
-
-    if teller1 != 0 or teller2 != 0 or teller3 != 0:
-        gemiddelde1 = (som1/teller1)
-        gemiddelde2 = (som2/teller2)
-        gemiddelde3 = (som3/teller3)
     
     for i in range(3):
         # calculate the 3D coordinates of the point
@@ -278,4 +305,4 @@ def main(aantal):
     return output
 
 
-getImages()
+print(main())
