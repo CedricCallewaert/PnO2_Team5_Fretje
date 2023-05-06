cdd068c580def5d3b9ea1e09b626e63de259d02a
Maxime Van Insberghe <maxime.vaninsberghe@student.kuleuven.be>
2023-05-06 20:21:53 +0200

    K-means clustering

--- LabVIEW/Fredje - LabVIEW Project/Cameradetectie/Color_Detection.py
+++ LabVIEW/Fredje - LabVIEW Project/Cameradetectie/Color_Detection.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 from time import sleep
 import os
-
+from sklearn.cluster import KMeans
 
 # global variables
 cameraNr = 0
@@ -16,17 +16,29 @@ def get_coordinates():
     with np.load("homography.npz") as X:
         H = X["homography_matrix"]
 
-    # calculate 3D coordinates
-    point_2D=np.array([xGem, yGem, 1])
-    point_3D = np.dot(H, point_2D)
-    point_3D /= point_3D[2]
+    # load coordinates
+    coordinates= calculate_triplets()
 
-    output = calculate_angles(point_3D)
+    # calculate 3D coordinates
+    output = []
+    for tuple in coordinates:
+        ls = []
+        x=tuple[0]
+        y=tuple[1]
+
+        point_2D= np.array([x, y, 1])
+        point_3D = np.dot(H, point_2D)
+        point_3D /= point_3D[2]
+
+        angle_distance, new_2D = calculate_angles(point_3D)
+
+        ls.append(angle_distance, [x,y], new_2D)
+        output.append(ls)
     
 
-    return output, [xGem, yGem], point_3D
+    return output
 
-def draw_circle(contour, frame):
+def draw_circle(contour, frame, number):
     
     c = contour
     # transform into circle
@@ -50,7 +62,8 @@ def draw_circle(contour, frame):
     cv2.circle(frame, center1, 5, (255, 0, 255), -1)
 
     #create text
-    s1 = "x1: {}, y1: {}, radius1: {}".format(np.round(x1), np.round(y1), np.round(radius1))
+    s1 = "x1: {}, y1: {}".format(np.round(x1), np.round(y1))
+
     # write to the screen
     cv2.putText(frame, s1, (25, 50 + number * 25), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
 
@@ -66,8 +79,6 @@ def change_exposure(x):
 	global exposure
 	exposure = x
         
-
-
 def get_frame():
     global cap1
     global previousExposure
@@ -81,10 +92,10 @@ def get_frame():
         cap1.set(cv2.CAP_PROP_EXPOSURE,exposure)
 
     _, frame = cap1.read()
+
     # convert to hsv
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    
     # define range of red color in HSV
     lower_red_1 = np.array([0,0,220]) 
     upper_red_1 = np.array([179,50,255])
@@ -95,51 +106,58 @@ def get_frame():
     sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
     
     if len(contours) > 0:
-        x1, y1 = draw_circle(sorted_contours[0], frame)
+        x1, y1 = draw_circle(sorted_contours[0], frame, 0)
     if len(contours) > 1:
-        x2, y2 = draw_circle(sorted_contours[1], frame)
+        x2, y2 = draw_circle(sorted_contours[1], frame, 1)
     if len(contours) > 2:
-        x3, y3 = draw_circle(sorted_contours[2], frame)
+        x3, y3 = draw_circle(sorted_contours[2], frame, 2)
     if len(contours) > 3:
-        x4, y4 = draw_circle(sorted_contours[3], frame)
+        x4, y4 = draw_circle(sorted_contours[3], frame, 3)
     if len(contours) > 4:
-        x5, y5 = draw_circle(sorted_contours[4], frame)
+        x5, y5 = draw_circle(sorted_contours[4], frame, 4)
     if len(contours) > 5:
-        x6, y6 = draw_circle(sorted_contours[5], frame)
+        x6, y6 = draw_circle(sorted_contours[5], frame, 5)
     if len(contours) > 6:
-        x7, y7 = draw_circle(sorted_contours[6], frame)
+        x7, y7 = draw_circle(sorted_contours[6], frame, 6)
     if len(contours) > 7:
-        x8, y8 = draw_circle(sorted_contours[7], frame)
+        x8, y8 = draw_circle(sorted_contours[7], frame, 7)
     if len(contours) > 8:
-        x9, y9 = draw_circle(sorted_contours[8], frame)
-
-    
-
+        x9, y9 = draw_circle(sorted_contours[8], frame, 8)
 
     #show the image for x mseconds before it automatically closes
-    #cv2.waitKey(1) 
-def distance(p1, p2):
-    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+    cv2.waitKey(1) 
 
 def calculate_triplets():
-    coordinates= [(x1, y1), (x2, y2), (x3, y3), (x4, y4), (x5, y5), (x6, y6), (x7, y7), (x8, y8), (x9, y9)]
-    coordinates= coordinates[:len(contours)]
-    close_triplets = []
-    threshold = 80
+    # load coordinates
+    coordinates= np.array([(x1, y1), (x2, y2), (x3, y3), (x4, y4), (x5, y5), (x6, y6), (x7, y7), (x8, y8), (x9, y9)])
 
-    # Loop over every possible combination of three points
-    for i in range(len(coordinates)):
-        for j in range(i+1, len(coordinates)):
-            for k in range(j+1, len(coordinates)):
-                # Calculate the distances between the three points
-                d1 = distance(coordinates[i], coordinates[j])
-                d2 = distance(coordinates[i], coordinates[k])
-                d3 = distance(coordinates[j], coordinates[k])
-                # If all three distances are less than the threshold, add this triplet to the list
-                if d1 < threshold and d2 < threshold and d3 < threshold:
-                    close_triplets.append((coordinates[i], coordinates[j], coordinates[k]))
+    # def number of contours
+    number_of_contours = len(contours)
 
+    # remove coordinates if there are less than 3 / 6 / 9 contours
+    if number_of_contours >= 9:
+        coordinates = coordinates
+        cluster_number = 3
+    elif number_of_contours >= 6:
+        coordinates = coordinates[:6]
+        cluster_number = 2
+    elif number_of_contours >= 3:
+        coordinates = coordinates[:3]
+        cluster_number = 1
+    else:
+        coordinates = np.array([])
 
+    # Apply k-means clustering with 3 clusters
+    kmeans = KMeans(n_clusters=cluster_number, random_state=0).fit(coordinates)
+
+    # Find the highest y value in each cluster
+    highest_y_coordinates = []
+    for cluster in range(cluster_number):
+        cluster_points = coordinates[kmeans.labels_ == cluster]
+        highest_y_point = cluster_points[np.argmax(cluster_points[:, 1])]
+        highest_y_coordinates.append(tuple(highest_y_point))
+
+    return highest_y_coordinates
 
 def close_stream():
     global cap1
@@ -173,10 +191,9 @@ def click_event(event,x,y,flags,params):
         print(calibration_point)
         np.savez(f"points/calibration_point{ii}.npz", calibration_point=calibration_point)
         ii+=1
+
 def warp():
     # Capture a frame
-    
-
     with np.load("homography.npz") as X:
         H = X["homography_matrix"]
     
@@ -187,6 +204,7 @@ def warp():
         key = cv2.waitKey(1)
         if key == 27:
             break
+
 def click_test():
     global ii
     ii=0
@@ -215,11 +233,9 @@ def calculate_angles(point_3D):
     # convert to degrees
     alpha = np.rad2deg(alpha)
  
-
     return [alpha, distance_plane], [x,y]
 
 
-calculate_homogeneous_matrix()
 
 
             
